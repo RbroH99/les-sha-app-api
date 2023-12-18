3e4827ff88516cf254e2f1a2d2279dda1619bb35
RbroH99 <rbroh99@gmail.com>
2023-12-18 12:53:18 -0500

    Implemented Token API.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,11 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
@@ -26,10 +30,36 @@ class UserSerializer(serializers.ModelSerializer):
         """Validates the phone number."""
         pattern = re.compile(r"(\+\d{1,3})?\s?\(?\d{2}\)?[\s.-]?\d{3}[\s.-]?\d{3}")
         if not pattern.match(phone):
-            raise serializers.ValidationError("Invalid phone number.")
+            msg = _("Invalid phone number.")
+            raise serializers.ValidationError(msg)
 
         return phone
 
     def create(self, validated_data):
         """Create and return a new user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authnticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.VAlidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -86,4 +87,43 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_token(self):
+        """Test generates token for valid credentials."""
+        user_details = {
+            'email': 'test@example.com',
+            'password': 'pass123',
+            'name': 'Test Name',
+            'phone': '+5359103546',
+        }
+        create_user(**user_details)
+
+        payload = {
+            'email': user_details['email'],
+            'password': user_details['password'],
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials not valid."""
+        create_user(email='test@example.com', password='goodpass')
+
+        payload = {'email': '', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting blank password returns error."""
+        create_user(email='test@example.com', password='testpass123')
+
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
